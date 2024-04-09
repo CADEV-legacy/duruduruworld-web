d6d1b357e803d709b9eae93367191aa1b6827eeb
sana-lazystar <sana.lazystar@gmail.com>
2024-04-09 10:15:25 +0900

    Remove prefetch on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,23 @@
 
 import Link from 'next/link';
 
-import { HydrationBoundary, dehydrate } from '@tanstack/react-query';
-
-import { getQueryClient, userQueryOptions } from './(client)/service';
 import { ROUTE_URL } from './constant';
 import styles from './page.module.css';
 
 import { CommonLayout, SocketComponent } from '@/(client)/component';
 
-const prefetchData = async () => {
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery(userQueryOptions.count());
-
-  return dehydrate(queryClient);
-};
-
-const Page: React.FC = async () => {
-  const dehydratedState = await prefetchData();
-
+const Page: React.FC = () => {
   return (
     <CommonLayout>
-      <HydrationBoundary state={dehydratedState}>
-        <div className={styles.container}>
-          <section className={styles.box}>
-            <h1 className={styles.title}>Welcome to StarWalkin&apos;3D</h1>
-            <Link className={styles.goToPostButton} href={ROUTE_URL.post.prefix}>
-              글 보러가기
-            </Link>
-            <SocketComponent />
-          </section>
-        </div>
-      </HydrationBoundary>
+      <div className={styles.container}>
+        <section className={styles.box}>
+          <h1 className={styles.title}>Welcome to StarWalkin&apos;3D</h1>
+          <Link className={styles.goToPostButton} href={ROUTE_URL.post.prefix}>
+            글 보러가기
+          </Link>
+          <SocketComponent />
+        </section>
+      </div>
     </CommonLayout>
   );
 };
