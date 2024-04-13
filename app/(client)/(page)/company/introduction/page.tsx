08b5e4318e1558bff9162b36522770e500bdc495
sana-lazystar <sana.lazystar@gmail.com>
2024-04-13 21:45:50 +0900

    Updates for using snap

---
+++ app/(client)/(page)/company/introduction/page.tsx
@@ -0,0 +1,10 @@
+const Page: React.FC = () => {
+  return (
+    <div>
+      <h1>회사소개</h1>
+      <p>회사소개 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Page;
---
+++ app/(client)/(page)/company/service/page.tsx
@@ -0,0 +1,10 @@
+const Page: React.FC = () => {
+  return (
+    <div>
+      <h1>서비스소개</h1>
+      <p>서비스소개 페이지입니다.</p>
+    </div>
+  );
+};
+
+export default Page;
